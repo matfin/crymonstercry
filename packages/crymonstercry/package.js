2b8d9c8ca870676069effe70c9127406c4a1bfbc
Matt Finucane <matfin@gmail.com>
2015-01-13 14:31:39 +0100

    Fleshing out and adding cry monster cry package for managing server and client side functionality.

---
+++ packages/crymonstercry/package.js
@@ -0,0 +1,48 @@
+/**
+ *	Cry Monster Cry
+ *	Server and client side package for this app.
+ *	Matt Finucane 2015
+ */
+
+Package.describe({
+	summary: 	'This package contains client and server side code for fetching and publishing data.',
+	version: 	'0.0.1',
+	name:  		'crymonstercry' 
+});
+
+Package.onUse(function(api) {
+
+	/**
+	 *	Minimum version of Meteor Required
+	 */
+	api.versionsFrom('1.0.2.1');
+
+	/**
+	 *	Other packages as requirements
+	 */
+	api.use('underscore', 'server');
+	api.use('http', 'server');
+	
+	/**
+	 *	Server side files included as part of this package
+	 */
+	api.addFiles([
+		'_server/contentful.js',
+		'_server/instagram.js',
+		'_server/tumblr.js',
+		'_server/youtube.js',
+		'_server/server.js'
+	], 	'server');
+
+	/**
+	 *	Client side files included as part of this package
+	 */
+	api.addFiles([
+		'_client/app.js'
+	], 	'client')
+
+	/**
+	 *	Export the above data fetchers as objects
+	 */
+
+});

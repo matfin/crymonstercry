8d61fed8c8b817f90e0a986eebe982f6f8c52851
Matt Finucane <matfin@gmail.com>
2015-01-15 18:11:44 +0100

    Removed temporary landing page. Fixed styling for social and top nav links.

--- client/helpers/ui_helpers.js
+++ client/helpers/ui_helpers.js
@@ -8,8 +8,29 @@
  *	Function to return the data for the top navigation
  *	
  *	@function 	topNav
- *	@return 	{Object} - an object containing the top navigation
+ *	@return 	{Object} - an object containing the data for top navigation links
  */
 UI.registerHelper('topNav', function() {
 	return App.nav.top;
+});
+
+/**
+ *	Function to return the data for the social navigation
+ *	
+ *	@function 	socialNav
+ *	@return 	{Object} - an object containing the data for social links
+ */
+UI.registerHelper('socialNav', function() {
+	return App.nav.social;
+});
+
+/**
+ *	Function to return a lowercased string, useful for class names
+ *	
+ *	@function 	loweCase
+ *	@param 		{String} - the string that needs to be converted to lowercase
+ *	@return		{String} - a lowercase string
+ */
+UI.registerHelper('lowerCase', function(str) {
+	return str.toLowerCase();
 });
